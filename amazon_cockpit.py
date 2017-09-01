9014a8e92d45e8673bae1368efed247e7c81cd3f
root <root@Web1.sybl.eu>
2017-09-01 04:50:29 +0000

    Just added the script

---
+++ amazon_cockpit.py
@@ -0,0 +1,12 @@
+#!/root/amazon_stock_hunter/amazon_hunter_script/amazon_cockpit/amazon_cockpit/bin/python
+
+import time
+
+flag = True
+
+while flag:
+   print "working"
+   flag = bool(raw_input(bool))
+   time.sleep(1)
+
+print "Finished"
