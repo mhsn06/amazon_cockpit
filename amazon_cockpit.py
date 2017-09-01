6aa8ebfbbbec160bb07885b8b91995d0f4566d30
Hassan <mhsn06@gmail.com>
2017-09-01 13:02:32 +0600

    Added start stop status action

--- amazon_cockpit.py
+++ amazon_cockpit.py
@@ -1,7 +1,7 @@
 #!/root/amazon_stock_hunter/amazon_hunter_script/amazon_cockpit/amazon_cockpit/bin/python
 
 import time
-
+from subprocess import call
 
 """
 Menu Items
@@ -43,6 +43,31 @@ def select_menu():
         except:
             print "\nERROR: Invalid input.\n"
 
+            
+def start_scraper():
+    print "starting the scraper"
+        #todo start the scraper
+    
+    call(["systemctl", "start", "amazon.service"])
+
+    print "scraper started"
+    
+
+def pause_scraper():
+    pass
+
+def stop_scraper():
+    call(["systemctl", "stop", "amazon.service"])
+
+def resume_scraper():
+    pass
+
+def show_live():
+    pass
+
+def get_scraper_status():
+    call(["systemctl", "status", "amazon.service"])
+            
 while True:
     print welcome_message
     
@@ -51,6 +76,8 @@ while True:
     if selection == 0:
         print farewell_message
         break
+    elif selection == 1:
+        pass
     else:
         print "you have selected " +  menu_items[selection]
     
